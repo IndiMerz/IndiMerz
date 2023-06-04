086c86b7f6d244183da7eb30dee1e7c32f994847
shahbaz4783 <shahbaz4783@gmail.com>
2023-06-05 01:22:13 +0530

    array methods

--- javascript/05 Arrays and Objects/02_array-methods.js
+++ javascript/05 Arrays and Objects/02_array-methods.js
@@ -1,2 +1,26 @@
+// 1. Popping and Pushing
+// pop() removes the last element from an array
+// push() adds a new element to an array at the end
 
- 
+const myFavBrands = ['Apple', 'Oneplus', 'Oppo', 'Samsung'];
+
+let remove = myFavBrands.pop();
+console.log(myFavBrands); // [ 'Apple', 'Oneplus', 'Oppo' ]
+
+let add = myFavBrands.push('Vivo');
+console.log(myFavBrands); // [ 'Apple', 'Oneplus', 'Oppo', 'Vivo' ]
+
+
+// 2. Shifting and Unshifting
+// shift() removes the first array element
+// unshift() adds a new element to an array at the beginning
+
+const vegetables = ['Potato', 'Tomato', 'Brinjal'];
+
+vegetables.shift();
+console.log(vegetables);  // [ 'Tomato', 'Brinjal' ]
+
+vegetables.unshift('Cabbage');
+console.log(vegetables);  // [ 'Cabbage', 'Tomato', 'Brinjal' ]
+
+
