c827594ba128f3e538c8b5c4057d079004cebfec
shahbaz4783 <shahbaz4783@gmail.com>
2023-06-11 19:05:54 +0530

    for loop

---
+++ javascript/08 Loops/01_for-loop.js
@@ -0,0 +1,83 @@
+for (let i = 0; i <=10; i++) {
+    console.log(i);
+}
+
+
+// get odd num
+for (let i = 1; i <=10; i+=2) {
+    console.log('Odd: ' + i);
+}
+
+// get even num
+for (let i = 1; i <=10; i+=2) {
+    console.log('Even: ' + i);
+}
+
+// if else in a loop
+for(let i=0; i<=10; i++){
+    if (i%2 === 0){
+        console.log('Even: ' + i);
+        
+    } else {
+        console.log('Odd: ' + i);
+    }
+}
+
+const names = ['Henry', 'Ross', 'Sarah', 'David', 'Kevin']
+
+for (let i = 0; i < names.length; i++) {
+    if (names[i] === 'Sarah' ){
+        console.log(`${names[i]} is a girl`);
+    } else{
+        console.log(names[i]);
+    }
+}
+
+
+
+// break
+for (let i = 0; i <= 10; i++){
+    if (i === 7){
+        console.log('Stopping Now...');
+        break
+    } else{
+        console.log(i);
+    }
+}
+/*
+0
+1
+2
+3
+4
+5
+6
+Stopping Now...
+*/
+// it will stop the loop if condition match
+
+
+
+// continue
+for (let i = 0; i <= 10; i++){
+    if (i === 7){
+        console.log('Stopping Now...');
+        continue
+    } else{
+        console.log(i);
+    }
+}
+/*
+0
+1
+2
+3
+4
+5
+6
+Stopping Now...
+8
+9
+10
+*/
+// it doenst stops if condition match, gives output and resume loop
