fdc3edf0c18668858e5b38a616b291b56d384e7c
shahbaz4783 <shahbaz4783@gmail.com>
2023-07-05 14:38:20 +0530

    cansvas line, image draw

--- javascript/13 Web API/code/03_canvas.js
+++ javascript/13 Web API/code/03_canvas.js
@@ -3,9 +3,36 @@ const ctx = canvas.getContext('2d');
 
 // Draw Rectangle
 ctx.fillStyle = 'blue';
-ctx.fillRect(10, 20, 30, 70);
+ctx.fillRect(10, 20, 130, 70);
 
 // Draw Circle
 ctx.fillStyle = 'green';
-ctx.arc(300, 300, 100, 0, Math.PI * 2)
-ctx.fillStyle();
+ctx.arc(300, 300, 50, 0, Math.PI * 2)
+ctx.fill();
+
+// Draw lines
+ctx.beginPath();
+ctx.strokeStyle = 'black';
+ctx.moveTo(10, 20);
+ctx.lineTo(300, 300);
+ctx.lineWidth = 4;
+ctx.stroke();
+
+
+// Draw Text
+ctx.font = '25px Times New Roman';
+ctx.fillStyle = 'red';
+ctx.fillText('Text on Canvas', 400, 150, 100);
+
+// Draw Stroke Text
+ctx.font = '40px Cursive';
+ctx.lineWidth = 2;
+ctx.strokeStyle = 'pink'
+ctx.strokeText('Stroke Text', 200, 400, 100);
+
+
+// Draw Image
+const image = document.getElementById('football');
+image.style.display = 'none';
+
+image.addEventListener('load', () => ctx.drawImage(image, 200, 250, 100, 100));
