649d46fa6812fe29b1a05c6651beb8f9b1867129
shahbaz4783 <shahbaz4783@gmail.com>
2023-07-09 15:59:02 +0530

    private class fields

--- javascript/14 OOP/code/07_incapacitation.js
+++ javascript/14 OOP/code/07_incapacitation.js
@@ -67,3 +67,62 @@ wallet.withdraw(120);
 console.log(wallet);
 console.log(wallet.balance);
 console.log(wallet.transaction);
+
+
+// Private Class Fields
+
+class Wallet2 {
+
+    #balance = 0;
+    #transaction = [];
+    
+
+    deposit(amount){
+        this.#processDeposit(amount);
+        this.#balance += amount;
+    }
+
+    withdraw(amount){
+        if (amount > this.#balance){
+            console.log('Transaction Failed: Not Enough Funds available');
+            return;
+        }
+        this.#processWithdraw(amount);
+        this.#balance -= amount;
+    }
+
+    #processDeposit(amount){
+        console.log(`Depositing ${amount}`);
+
+        this.#transaction.push({
+            type: 'deposit',
+            amount,
+        })
+    }
+
+    #processWithdraw(amount){
+        console.log(`Withdrawing ${amount}`);
+
+        this.#transaction.push({
+            type: 'withdraw',
+            amount,
+        })
+    }
+
+    get balance() {
+        return this.#balance;
+    }
+
+    get transaction() {
+        return this.#transaction;
+    }
+}
+
+
+const wallet2 = new Wallet2();
+wallet2.deposit(500);
+wallet2.withdraw(320);
+
+console.log(wallet2);
+console.log(wallet2.balance);
+console.log(wallet2.transaction);
