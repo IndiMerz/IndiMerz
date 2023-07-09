9703c2ad0f8f21137dfc7a53a9b8527114bab659
shahbaz4783 <shahbaz4783@gmail.com>
2023-07-09 14:53:12 +0530

    getters and setters with constructor and literals

--- javascript/14 OOP/code/06_getters-setters.js
+++ javascript/14 OOP/code/06_getters-setters.js
@@ -41,3 +41,75 @@ peep1.lName = 'lewis'
 console.log(peep1.fullName);
 
 console.log(peep1);
+
+
+// Getters and setters with object constructor
+
+function Animal (name, type) {
+    this._name = name;
+    this._type = type;
+
+    Object.defineProperty(this, 'name', {
+        get: function(){
+            return this.capFstLett(this._name);
+        },
+        set: function(value){
+            this._name = value;
+        }
+    });
+
+    Object.defineProperty(this, 'type', {
+        get: function(){
+            return this.capFstLett(this._type);
+        },
+        set: function(value){
+            this._type = value;
+        }
+    });
+
+    Object.defineProperty(this, 'detail', {
+        get: function(){
+            return `${this.name} is a ${this._type} animal`;
+        }
+    });
+}
+
+Animal.prototype.capFstLett = function (value){
+    return value.charAt(0).toUpperCase() + value.slice(1)
+}
+
+const creature = new Animal('lion', 'wild');
+console.log(creature);
+console.log(creature.name);
+console.log(creature.type);
+console.log(creature.detail);
+
+
+// Getters and setters with object literal
+
+const AnimalObj = {
+    _name: 'Horse',
+    _type: 'Domestic',
+
+    get name() {
+        return Animal.prototype.capFstLett(this._name);
+    },
+
+    set name(value) {
+        return this._name = value;
+    },
+
+    get type() {
+        return Animal.prototype.capFstLett(this._type);
+    },
+
+    set type(value) {
+        return this._type = value;
+    }
+}
+
+
+const creature2 = Object.create(AnimalObj);
+console.log(creature2);
+console.log(creature2.name);
+console.log(creature2.type);
