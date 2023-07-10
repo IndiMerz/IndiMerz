f68be6ce687a6907e45ce34421bb502fbfe30181
shahbaz4783 <shahbaz4783@gmail.com>
2023-07-10 14:59:11 +0530

    webpack

---
+++ javascript/15 Modules & Tooling/code/04 Module Bundlers/webpack/src/index.js
@@ -0,0 +1,6 @@
+import message from './message.js';
+import './css/style.css';
+
+console.log(message);
+console.log(message.text);
+
---
+++ javascript/15 Modules & Tooling/code/04 Module Bundlers/webpack/src/message.js
@@ -0,0 +1,7 @@
+const message = {
+    year: '2023',
+    month: 'July',
+    text: 'It is rainy season'
+}
+
+export default message;
