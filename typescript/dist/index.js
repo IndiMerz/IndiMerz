de000c9890ff5a4864cc77a780b2466e068121d2
shahbaz4783 <shahbaz4783@gmail.com>
2023-07-22 10:26:36 +0530

    typescript

---
+++ typescript/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+// Basic Types
+let id = 10;
+let profession = 'developer';
+let isActive = true;
+// any can be converted to any type later
+let random = 'hello';
+random = false;
---
+++ typescript/src/index.ts
@@ -0,0 +1,8 @@
+// Basic Types
+let id: number = 10;
+let profession: string = 'developer';
+let isActive: boolean = true;
+
+// any can be converted to any type later
+let random: any = 'hello';
+random = false
