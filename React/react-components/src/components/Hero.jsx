26aebc347f421cac05881d0e00cc1aa6d25c4e5f
shahbaz4783 <shahbaz4783@gmail.com>
2023-07-31 01:04:16 +0530

    react components

---
+++ React/react-components/src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Hero = () => {
+  return (
+    <section>
+        <h2>Top Javascript Libraries:</h2>
+        <ul>
+            <li>React</li>
+            <li>Angular</li>
+            <li>Vue</li>
+            <li>Svelte</li>
+        </ul>
+    </section>
+  )
+}
